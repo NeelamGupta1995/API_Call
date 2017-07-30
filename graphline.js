e5b4e5e8b0450ec02b2ab9027b47e6a609f045fd
NeelamGupta1995 <gupta.neelam.sn@gmail.com>
2017-07-30 03:01:46 -0700

    feedback and attendance API

--- graphline.js
+++ graphline.js
@@ -1,5 +1,5 @@
 var testApp = angular.module('testApp',[]);
-testApp.controller('detailsController',function($scope,$http)
+testApp.controller('attendanceController',function($scope,$http)
  {
 	 
 	 
@@ -15,6 +15,34 @@ testApp.controller('detailsController',function($scope,$http)
 		obj=response;
 		console.log(obj);
 		attendance_array = obj['data']['result'];
+		
+		
+		
+	})
+	
+	
+	
+	
+	
+	
+ });
+ 
+ testApp.controller('feedbackController',function($scope,$http)
+ {
+	 
+	 
+	$http({
+		'method':'GET',
+		'url':'http://acadview.getsandbox.com/feedback',
+		'headers':{
+			'Content-Type': 'application/json'
+		},
+	}).then(function(response){
+		$scope.obj={}
+		
+		obj=response;
+		console.log(obj);
+		feedback_array = obj['data']['result'];
 		
 		
 		
